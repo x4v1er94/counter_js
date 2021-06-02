29677d5f24a6537d0201936f584e37bf93e572d5
x4v1er94 <saverio.bertocci@gmail.com>
2021-06-02 12:22:50 +0200

    ultimazione javascript

--- assets/js/codes.js
+++ assets/js/codes.js
@@ -1,19 +1,36 @@
 
-var inputText = document.getElementById('inputText');
-console.log (inputText);
-inputText.setAttribute("value","0");
+var count = 0;
+
+//legge il valore di value della casella di testo al caricamento della pagina
+window.addEventListener("load",readValue);
+
+function readValue(){
+  var inputText = document.getElementById("inputText");
+  inputText.disabled = true;
+  count = inputText.value;
+}
+
+//aggiorna il valore di value
+function updateValue(){
+  var inputText = document.getElementById("inputText");
+  inputText.value = count;
+
+}
+
 
 function buttonPlus(){
+  ++count;
+  updateValue();
 
-
-}
+};
 
 function buttonMinus(){
+  --count;
+  updateValue();
 
-
-}
+};
 
 function buttonReset() {
-
-
-}
+  count = 0
+  updateValue();
+};
